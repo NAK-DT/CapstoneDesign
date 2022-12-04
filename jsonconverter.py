cd15304f73dba96fa51313e6f862034e0b5eafd4
NAK-DT <104759139+NAK-DT@users.noreply.github.com>
2022-12-04 00:19:59 -0800

    Add files via upload

---
+++ jsonconverter.py
@@ -0,0 +1,27 @@
+import shutil
+import os
+import json
+import re
+from ast import parse
+from ast2json import ast2json
+
+dir_path = "/home/skdanski/Documents/capstone_project/Project_CodeNet_Python800"
+regex = r'(?<!: )"(\S*?)"'
+
+# create the AST and save it in a .json file
+def createAst(file, filename, dest_dir):
+    """docstring"""
+    ast = ast2json(parse(open(file).read()))
+    jsonString = re.sub(regex, '\\1', json.dumps(ast, indent=4))
+    jsonFile = open(dest_dir + "/%s.json" % filename, "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+for subfolder in next(os.walk(dir_path))[1]:
+    print("Going through: " + subfolder)
+    for file in os.listdir(os.path.join(dir_path, subfolder)):
+        try:
+            createAst(file=os.path.join(os.path.join(dir_path, subfolder), file), filename=os.path.splitext(file)[0], dest_dir=os.path.join(dir_path, subfolder))
+        except:
+            continue
+
